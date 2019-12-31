c1e17376e86f8077acb1e5c8c7665c6a6503f13e
praveen-edulakanti <praveen.edulakanti@gmail.com>
2019-12-31 15:34:37 +0530

    user data updated

--- user-data-prv1.sh
+++ user-data-prv1.sh
@@ -6,8 +6,6 @@ sudo a2enmod rewrite
 sudo a2enmod ssl
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo git config --global user.name "praveen-edulakanti"
-sudo git config --global user.email "praveen.edulakanti@gmail.com"
 sudo mkdir /var/www/html/user
 sudo git clone https://github.com/praveen-edulakanti/user.git /var/www/html/user
 sudo mkdir /var/www/html/dashboard
